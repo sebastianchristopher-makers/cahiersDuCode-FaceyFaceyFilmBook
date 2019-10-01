288a20358b1f93240003f9eca8c1f20f302ea7d5
assadk88 <assadkhan@outlook.com>
2019-10-01 14:46:58 +0100

    Add hardcoded films for films to pass to the view

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ get '/user_profile' do
 end
 
 get '/user_profile/watched' do
-  @films = [{
+  ungroupedFilms = [{
     image:"https://contentserver.com.au/assets/598411_p173378_p_v8_au.jpg",
     title: "The Dark Knight",
     year: "2008",
@@ -46,6 +46,7 @@ get '/user_profile/watched' do
     year: "2008",
     synopsis: "With the help of allies Lt. Jim Gordon (Gary Oldman) and DA Harvey Dent (Aaron Eckhart), Batman (Christian Bale) has been able to keep a tight lid on crime in Gotham City. But when a vile young criminal calling himself the Joker (Heath Ledger) suddenly throws the town into chaos, the caped Crusader …"
   }]
+  @films = ungroupedFilms.each_slice(3).to_a
   erb :_watched
 end
 
