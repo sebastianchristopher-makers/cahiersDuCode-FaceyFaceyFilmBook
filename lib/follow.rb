3d1efb16aca18d9aba31855c069e7a688962a22d
claredowson <clare.dowson@barnardos.org.uk>
2019-10-08 16:39:38 +0100

    follows model

--- lib/follow.rb
+++ lib/follow.rb
@@ -21,8 +21,20 @@ class Follower
 
     def self.find_by_user_id(user_id)
         rs = DatabaseConnection.query("SELECT * FROM followers WHERE userID = #{user_id}")
-        rs.map do |follow| 
+        rs.map do |follow|
+        # what happens if rs returns 0 results
              Follower.new(follow["id"].to_i, follow["user_id"].to_i, follow["follower_id"].to_i)
         end
     end
+
+    def self.get_followers(user_id) # number of followers a user has
+      rs = DatabaseConnection.query("SELECT COUNT(followerid) AS followers FROM followers WHERE followerid = #{user_id};")
+      return rs[0]["followers"].to_i
+    end
+
+    def self.get_following(user_id) # number of people a user is following
+      rs = DatabaseConnection.query("SELECT COUNT(userid) AS following FROM followers WHERE userid = #{user_id};")
+      return rs[0]["following"].to_i
+    end
+
 end
--- spec/follower_spec.rb
+++ spec/follower_spec.rb
@@ -19,9 +19,32 @@ describe Follower do
         follower3 = Follower.add(user1.id, user4.id)
         expect(Follower.find_by_user_id(1)).to contain_exactly(follower1, follower2, follower3)
     end
+
+    it 'can return the number of people following the user' do
+      user1 = User.create("test1@test.com", "pass")
+      user2 = User.create("test2@test.com", "pass")
+      user3 = User.create("test3@test.com", "pass")
+      user4 = User.create("test4@test.com", "pass")
+      follower1 = Follower.add(user1.id, user2.id)
+      follower2 = Follower.add(user1.id, user3.id)
+      follower3 = Follower.add(user1.id, user4.id)
+      expect(Follower.get_following(1)).to eq(3)
+    end
+
+    it 'can return the number of followers a user has' do
+      user1 = User.create("test1@test.com", "pass")
+      user2 = User.create("test2@test.com", "pass")
+      user3 = User.create("test3@test.com", "pass")
+      user4 = User.create("test4@test.com", "pass")
+      follower1 = Follower.add(user1.id, user2.id)
+      follower2 = Follower.add(user1.id, user3.id)
+      follower3 = Follower.add(user1.id, user4.id)
+      expect(Follower.get_followers(1)).to eq(0)
+    end
+
     after(:each) do
-        DatabaseConnection.query('TRUNCATE users RESTART IDENTITY CASCADE;')
-        DatabaseConnection.query('TRUNCATE followers RESTART IDENTITY CASCADE;')
+        # DatabaseConnection.query('TRUNCATE users RESTART IDENTITY CASCADE;')
+        # DatabaseConnection.query('TRUNCATE followers RESTART IDENTITY CASCADE;')
 
       end
 end
