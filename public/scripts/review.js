ad646611d8d20fe256862feb07baf605da4c99d3
sebastianchristopher <christophersebastianjohnson@gmail.com>
2019-10-09 15:22:39 +0100

    fixes review box bug

--- public/scripts/review.js
+++ public/scripts/review.js
@@ -1,8 +1,14 @@
-// Get the modal
-let modal = document.getElementById("myModal");
-
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+window.onload = function(){
+  let spans = document.getElementsByClassName("close");
+}
+spans = document.getElementsByClassName("close");
+// let spans = document.getElementsByClassName("close")
+for(let i = 0; i < document.getElementsByClassName("close").length; i++){
+  spans[i].onclick = function() {
+    this.parentElement.parentElement.style.display = "none";
+  }
+}
 
 // When the user clicks the button, open the modal
 function openAddReview(id){
@@ -10,17 +16,12 @@ function openAddReview(id){
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 function deleteWatched(userId, filmId){
   $.post('/delete-watched', {userId:userId,filmId:filmId}, function(data){})
